f37d694ff656b4cd1a83630963b67c7f549be793
Beata <61662751+beatass@users.noreply.github.com>
2026-07-29 13:56:23 +0200

    Add files via upload

---
+++ insert_data.sql
@@ -0,0 +1,32 @@
+-------------------------------- Bank Data Analysis -----------------------------------
+
+/* INSERT INTO TABLES VALUES*/
+
+INSERT INTO Customer VALUES
+(1, 'standart', 25, 1900),
+(2, 'premium',  34, 2600),
+(3, 'VIP', 44, 5000),
+(4, 'standart', 28, 4500),
+(5, 'premium', 35, 5000),
+(6, 'VIP', 42, 11000)
+
+
+INSERT INTO Transactions VALUES
+(1, 'incoming', 3000, 1, 2019),
+(1, 'outgoing', 500, 1, 2019),
+(1, 'incoming', 700, 2, 2019),
+(1, 'outgoing', 400, 2, 2019),
+(1, 'outgoing', 199, 1, 2019),
+(2, 'incoming', 1500, 3, 2019),
+(2, 'incoming', 1350, 1, 2019),
+(2, 'outgoing', 2000, 2, 2019),
+(2, 'outgoing', 750, 3, 2019),
+(2, 'incoming', 250, 3, 2019),
+(2, 'outgoing', 800, 10, 2018),
+(2, 'outgoing', 400, 11, 2018),
+(2, 'incoming', 6500, 11, 2018),
+(3, 'incoming', 2400, 1, 2019),
+(3, 'incoming', 2000, 2, 2019),
+(3, 'incoming', 1600, 3, 2019),
+(5, 'outgoing', 4500, 4, 2019),
+(6, 'incoming', 12000, 4, 2019)
